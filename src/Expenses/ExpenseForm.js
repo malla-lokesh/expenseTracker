6184b23b18f984822b6b6ff758dca0474874a711
malla-lokesh <121810307046@gitam.in>
2023-05-29 22:53:47 +0530

    Completed Task: Editing and Deleting Expenses

--- src/Expenses/ExpenseForm.js
+++ src/Expenses/ExpenseForm.js
@@ -9,6 +9,8 @@ const ExpenseForm = () => {
     // const expenseContext = useContext(ExpenseContext);
     const [expenses, setExpenses] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
+    const [editMode, setEditMode] = useState(false);
+    const [editExpenseId, setEditExpenseId] = useState('');
 
     useEffect(() => {
         fetchExpenses();
@@ -18,9 +20,12 @@ const ExpenseForm = () => {
         setIsLoading(true);
         try {
             const response = await axios.get('https://expensetrackerstorage-default-rtdb.firebaseio.com/expenses.json')
+            if(response.data === null) {
+                setIsLoading(false);
+                return;
+            }
             const arrayObject = Object.entries(response.data);
-            const expenseArray = arrayObject.map(array => array[1]);
-            setExpenses(expenseArray);
+            setExpenses(arrayObject);
         } catch (error) {
             console.error('Error fetching expenses: ', error);
         }
@@ -31,7 +36,6 @@ const ExpenseForm = () => {
         event.preventDefault();
 
         const expense = {
-            id: Math.random(),
             amount: amount,
             description: description,
             category: category
@@ -41,16 +45,51 @@ const ExpenseForm = () => {
         setDescription('');
         setCategory('Choose a category');
 
-        try {
-            const response = await axios.post('https://expensetrackerstorage-default-rtdb.firebaseio.com/expenses.json', expense)
-            if(response.statusText !== 'OK') {
-                alert('sending expense to firebase failed!');
-            } else {
-                fetchExpenses();
+        if(editMode) {
+            try {
+                const response = await axios.put(`https://expensetrackerstorage-default-rtdb.firebaseio.com/expenses/${editExpenseId}.json`, expense);
+                if(response.statusText === 'OK') {
+                    fetchExpenses();
+                } else {
+                    throw new Error(response.data.error);
+                }
+            } catch(error) {
+                alert(error);
             }
-        } catch(error) {
-            console.error('Error : ', error);
+            setEditMode(false);
+        } else {
+            try {
+                const response = await axios.post('https://expensetrackerstorage-default-rtdb.firebaseio.com/expenses.json', expense)
+                if(response.statusText !== 'OK') {
+                    alert('sending expense to firebase failed!');
+                } else {
+                    fetchExpenses();
+                }
+            } catch(error) {
+                console.error('Error : ', error);
+            }
         }
+    }
+
+    const deleteExpenseHandler = (expenseId) => {
+        axios.delete(`https://expensetrackerstorage-default-rtdb.firebaseio.com/expenses/${expenseId}.json`)
+        .then(response => {
+            if(response.statusText === 'OK') {
+                fetchExpenses();
+            } else {
+                throw new Error(response.data.error);
+            }
+        }).catch(error => alert(error))
+    }
+
+    const editExpenseHandler = async (expenseItem) => {
+        const expenseId = expenseItem[0];
+        const expense = expenseItem[1];
+        setAmount(expense.amount);
+        setDescription(expense.description);
+        setCategory(expense.category);
+        setEditExpenseId(expenseId);
+        setEditMode(true);
     }
 
     return <React.Fragment>
@@ -64,16 +103,20 @@ const ExpenseForm = () => {
                 <option value='credit card'>Credit Card</option>
                 <option value='courses'>Courses</option>
             </select>
-            <button type='submit'>Add Expense</button>
+            <button type='submit'>{!editMode ? 'Add Expense' : 'Edit expense'}</button>
         </form>
         <hr/>
         {isLoading && <p>Loading your expenses...</p>}
-        {expenses!=null && expenses.map(expense => {
-            return <div key={expense.id}>
-                <div key={Math.random()}>
+        {expenses!=null && expenses.map(expenseItem => {
+            const expenseId = expenseItem[0];
+            const expense = expenseItem[1];
+            return <div key={expenseId}>
+                <div>
                     {expense.amount} - spent for {expense.category}
                 </div>
                 <div>{expense.description}</div>
+                <button type='button' onClick={() => {editExpenseHandler(expenseItem)}}>edit</button>
+                <button type='button' onClick={() => {deleteExpenseHandler(expenseId)}}>delete</button>
             </div>
         })}
     </React.Fragment>
